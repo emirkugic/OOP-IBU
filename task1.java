f9af8f6c4145e70df3df003a60ac4ee6df368e53
emirkugic <103369144+emirkugic@users.noreply.github.com>
2022-10-17 23:47:41 +0200

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.err.println("Emir Kugic");
+    }
+}
+ 
---
+++ task2.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.err.println("Hello World\nAnd all the people of the world");
+    }
+}
+ 
