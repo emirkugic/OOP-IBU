c54fc43ef9f80a00709314231a9cfe2ef713f3d9
emirkugic <103369144+emirkugic@users.noreply.github.com>
2022-10-25 23:18:36 +0200

    Delete task3.java

--- task3.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        int NumberOfSeconds = 365*24*60*60;
-
-        System.err.println("There are "+NumberOfSeconds+" seconds in a year");
-    }
-}
- 
