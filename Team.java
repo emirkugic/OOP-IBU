6d778df2eee25e9cda34f68b8cad4f16417f3ca2
emirkugic <103369144+emirkugic@users.noreply.github.com>
2022-12-11 20:26:10 +0100

    Update Team.java

--- Team.java
+++ Team.java
@@ -3,45 +3,55 @@ package week5;
 import java.util.ArrayList;
 
 public class Team {
-	private String name; 
-	private ArrayList<Player> players;
-	private int maxSize;
-	
-	public Team(String name) {
-		this.name = name;
-		this.players = new ArrayList<>();
-		this.maxSize = 16; 
-	}
-	
-	public String getName() {
-		return this.name; 
-	}
-	
-	public void addPlayer(Player player) {
-		if(this.size() <= this.maxSize) {
-			players.add(player);
-		}
-	}
-	
-	public void printPlayers() {
-		for(Player player : this.players) {
-			System.out.println(player);
-		}
-	}
-	
-	public void setMaxSize(int maxSize) {
-		this.maxSize = maxSize;
-	}
-	
-	public int size() {
-		return this.players.size();
-	}
-	
-	public int goals() {
-		int totalGoals = 0; 
-		for(Player player : this.players) {
-			totalGoals += player.getGoals();
-		}
-		return totalGoals;
-	}
+  private String name;                          			
+  private ArrayList<Player> players = new ArrayList<>();  	
+  private int maxSize = 30;                      			
+
+  // Constructor that takes the team's name as an argument
+  public Team(String name) {
+    this.name = name;
+  }
+
+  // Returns the team's name
+  public String getName() {
+    return name;
+  }
+
+  // Sets the team's name
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  // Adds a player to the team, if the number of players is less than the maximum size
+  public void addPlayer(Player player) {
+    if (players.size() < maxSize) {
+      this.players.add(player);
+    }
+  }
+
+  // Prints the names of all players on the team
+  public void printPlayers() {
+    for (int i = 0; i < players.size(); i++) {
+      System.out.println(players.get(i));
+    }
+  }
+
+  // Sets the maximum number of players on the team
+  public void setMaxSize(int maxSize) {
+    this.maxSize = maxSize;
+  }
+
+  // Returns the number of players on the team
+  public int size() {
+    return players.size();
+  }
+
+  // Returns the total number of goals for all players on the team
+  public int goals() {
+    int totalGoals = 0;
+    for (int i = 0; i < players.size(); i++) {
+      totalGoals += players.get(i).getGoals();
+    }
+    return totalGoals;
+  }
 }
